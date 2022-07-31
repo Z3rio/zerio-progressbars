3f75dc7273588e6614fef36c4e0dabaa6ce0c5df
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-07-31 21:58:32 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,14 @@
 Config = {}
 
 Config.Locale = "en"
-Config.NotifyEvent = "QBCore:Notify"
+
+if GetResourceState("qb-core") == 'started' then
+    Config.NotifyEvent = "QBCore:Notify"
+end
+if GetResourceState("es_extended") == 'started' then
+    Config.NotifyEvent = "esx:showNotification"
+end
+
+if Config.NotifyEvent == nil then
+    Config.NotifyEvent = ""
+end
